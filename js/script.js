c6aa209bed2d311c54d722d51b6921469d2ebe88
VasilyUA <VasilyUA@users.noreply.github.com>
2022-09-02 08:48:10 +0000

    deploy: 21206cf59fdeb17a6bb0741e73b20bb601ce7ecc

--- js/script.js
+++ js/script.js
@@ -81,8 +81,8 @@ function resize() {
 resize();
 window.addEventListener('resize', resize, true);
 ;
+// Check duplicate id in html
 const idsElement = document.querySelectorAll('[id]');
-
 const duplicates = [];
 idsElement.forEach(({ id }) => {
 	if (duplicates.includes(id)) {
@@ -91,4 +91,17 @@ idsElement.forEach(({ id }) => {
 		duplicates.push(id);
 	}
 });
+
+// support format webp for browsers
+function support_format_webp() {
+	const elem = document.createElement('canvas');
+
+	const isWebP = !!(elem.getContext && elem.getContext('2d'));
+	if (isWebP) return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0; // was able or not to get WebP representation
+	else return false; // very old browser like IE 8, canvas not supported
+}
+
+const isSuport = support_format_webp();
+const className = isSuport ? 'webp' : 'no-webp';
+document.documentElement.classList.add(className);
 ;
