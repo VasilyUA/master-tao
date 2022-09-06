b493da894d7746c8550c49b1b3fbb4f16a343c57
vasilyua <mironenkovasila@gmail.com>
2022-09-06 23:44:49 +0300

    Add slider card

--- src/js/slider-card.js
+++ src/js/slider-card.js
@@ -1,7 +1,7 @@
 let items = document.querySelectorAll('.slider-cards-element .carousel-item');
 
 items.forEach((el) => {
-	const minPerSlide = 4;
+	const minPerSlide = 3;
 	let next = el.nextElementSibling;
 	for (let i = 1; i < minPerSlide; i++) {
 		if (!next) {
