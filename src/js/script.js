5608aa5ebf4fb91c609be7bd983f2eb41caf0252
vasilyua <mironenkovasila@gmail.com>
2022-09-06 23:08:31 +0300

    Add slider card

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
+~include('dev.config.js');
 ~include('library/bootstrap.min.js');
 ~include('drag-and-drop.js');
 ~include('phone-slider.js');
-~include('dev.config.js');
+~include('slider-card.js');
