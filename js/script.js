0347ff3c5baaa1bd313468e02a082b7ab5fcb407
VasilyUA <VasilyUA@users.noreply.github.com>
2022-09-06 20:10:42 +0000

    deploy: 5608aa5ebf4fb91c609be7bd983f2eb41caf0252

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// Check duplicate id in html
+const idsElement = document.querySelectorAll('[id]');
+const duplicates = [];
+idsElement.forEach(({ id }) => {
+	if (duplicates.includes(id)) {
+		console.warn(`Duplicate id: ${id}`);
+	} else {
+		duplicates.push(id);
+	}
+});
+
+// support format webp for browsers
+function support_format_webp() {
+	const elem = document.createElement('canvas');
+
+	const isWebP = !!(elem.getContext && elem.getContext('2d'));
+	if (isWebP) return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0; // was able or not to get WebP representation
+	else return false; // very old browser like IE 8, canvas not supported
+}
+
+const isSuport = support_format_webp();
+const className = isSuport ? 'webp' : 'no-webp';
+document.documentElement.classList.add(className);
+;
 /*!
   * Bootstrap v5.2.0 (https://getbootstrap.com/)
   * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
@@ -88,27 +112,19 @@ function resize() {
 resize();
 window.addEventListener('resize', resize, true);
 ;
-// Check duplicate id in html
-const idsElement = document.querySelectorAll('[id]');
-const duplicates = [];
-idsElement.forEach(({ id }) => {
-	if (duplicates.includes(id)) {
-		console.warn(`Duplicate id: ${id}`);
-	} else {
-		duplicates.push(id);
+let items = document.querySelectorAll('.slider-cards-element .carousel-item');
+
+items.forEach((el) => {
+	const minPerSlide = 4;
+	let next = el.nextElementSibling;
+	for (let i = 1; i < minPerSlide; i++) {
+		if (!next) {
+			// wrap carousel by using first child
+			next = items[0];
+		}
+		let cloneChild = next.cloneNode(true);
+		el.appendChild(cloneChild.children[0]);
+		next = next.nextElementSibling;
 	}
 });
-
-// support format webp for browsers
-function support_format_webp() {
-	const elem = document.createElement('canvas');
-
-	const isWebP = !!(elem.getContext && elem.getContext('2d'));
-	if (isWebP) return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0; // was able or not to get WebP representation
-	else return false; // very old browser like IE 8, canvas not supported
-}
-
-const isSuport = support_format_webp();
-const className = isSuport ? 'webp' : 'no-webp';
-document.documentElement.classList.add(className);
 ;
